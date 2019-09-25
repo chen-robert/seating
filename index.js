5bcf1e916ff89e355c1a008d8852924023b79aa7
Robert <robertchen@live.com>
2019-09-25 23:03:01 +0000

    node-template

---
+++ index.js
@@ -0,0 +1,32 @@
+global.__rootdir = __dirname;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const autoprefixer = require('express-autoprefixer');
+const lessMiddleware = require('less-middleware');
+const cookieSession = require("cookie-session");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("trust proxy", "127.0.0.1");
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const staticPath = __dirname + '/public';
+app.use(lessMiddleware(staticPath));
+app.use(autoprefixer({browsers: ["last 3 versions", "> 1%"], cascade: false}));
+app.use(express.static(staticPath));
+
+app.get("/class/:name/:period", (req, res) => {
+  const {name, period} = req.params;
+
+  res.render("index", {
+    title: `${name} #${period}`
+  });
+});
+
+app.use((req, res) => res.redirect("/class/rhalfs/7"))
+
+app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
